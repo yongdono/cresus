3dace46021310eabd1ff02e369653a17e37a42fd
Joachim Naulet <jnaulet@rdinnovation.fr>
2016-04-01 15:38:43 +0200

    Added ATR

--- indicator/atr.c
+++ indicator/atr.c
@@ -4,12 +4,15 @@
 
 #include "atr.h"
 
-int atr_init(struct atr *a, int period, struct candle *cdl)
+int atr_init(struct atr *a, int period, const struct candle *c)
 {
+  /* Super() */
+  indicator_init(&a->parent, CANDLE_CLOSE, atr_feed);
+  
   a->period = period;
-  a->value = cdl->high - cdl->low;
-
-  memcpy(&a->last, cdl, sizeof *cdl);
+  a->value = c->high - c->low;
+  memcpy(&a->last, c, sizeof *c);
+  
   return 0;
 }
 
@@ -17,22 +20,25 @@ void atr_free(struct atr *a)
 {
 }
 
-double atr_feed(struct atr *a, struct candle *cdl)
+int atr_feed(struct atr *a, const struct candle *c)
 {
-  double max = cdl->high - cdl->low;
-  double h = fabs(cdl->high - a->last.close);
-  double l = fabs(cdl->low - a->last.close);
+  /* Compute "True Range" */
+  double tr = c->high - c->low;
+  double h = fabs(c->high - a->last.close);
+  double l = fabs(c->low - a->last.close);
+  
+  tr = (h > tr ? h : tr);
+  tr = (l > tr ? l : tr);
 
-  max = (h > max ? h : max);
-  max = (l > max ? l : max);
+  /* Average it */
   a->value = (a->value * (a->period - 1) + max) / a->period;
-
+  
   /* Remember last candle */
-  memcpy(&a->last, cdl, sizeof *cdl);
-  return a->value;
+  memcpy(&a->last, c, sizeof *c);
+  return 0;
 }
 
-double atr_compute(struct atr *a)
+double atr_value(struct atr *a)
 {
   return a->value;
 }
