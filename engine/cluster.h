3d300ce2ded19921213a2fea6efe3c15f6d3d51f
Joachim Naulet <jnaulet@rdinnovation.fr>
2016-04-20 16:41:20 +0200

    Prepared new cluster option ofr cresus

---
+++ engine/cluster.h
@@ -0,0 +1,28 @@
+/*
+ * Cresus EVO - cluster.h
+ *
+ * Created by Joachim Naulet <jnaulet@rdinnovation.fr> on 20/04/2016
+ * Copyright (c) 2016 Joachim Naulet. All rights reserved.
+ *
+ */
+
+#ifndef CLUSTER_H
+#define CLUSTER_H
+
+#include "framework/slist.h"
+#include "engine/timeline.g"
+
+struct cluster {
+  /* First a cluster's a list of timelines */
+  __slist_head__(struct timeline) slist_timeline;
+  /* Then, it's an indice computer */
+  /* Do we want some indicators on our cluster's data ? */
+};
+
+int cluster_init(struct cluster *c);
+void cluster_free(struct cluster *c);
+
+int cluster_add_timeline(struct cluster *c, struct timeline *t);
+int cluster_step(struct cluster *c);
+
+#endif
