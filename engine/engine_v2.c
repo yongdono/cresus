254047fc1a0799524965e7759d96dde1514e4a3b
Joachim Naulet <jnaulet@rdinnovation.fr>
2019-05-06 18:42:42 +0200

    Adding simpler engine for tests

---
+++ engine/engine_v2.c
@@ -0,0 +1,45 @@
+/*
+ * Cresus CommonTL - engine_v2.c
+ * 
+ * Created by Joachim Naulet <jnaulet@rdinnovation.fr> on 06/05/2019
+ * Copyright (c) 2014 Joachim Naulet. All rights reserved.
+ *
+ */
+
+#include "engine_v2.h"
+
+int engine_v2_init(struct engine_v2 *ctx, struct timeline *t)
+{
+  /* Timeline */
+  ctx->timeline = t;
+  /* Time */
+  ctx->start_time = TIME_MIN;
+  ctx->end_time = TIME_MAX;
+  /* Fees */
+  ctx->transaction_fee = 0;
+  /* Output */
+  ctx->csv_output = 0;
+    
+  return 0;
+}
+
+void engine_v2_release(struct engine_v2 *ctx)
+{
+}
+
+int engine_v2_set_common_opt(struct engine_v2 *ctx, struct common_opt *opt)
+{
+  if(opt->start_time.set) ctx->start_time = opt->start_time.t;
+  if(opt->end_time.set) ctx->end_time = opt->end_time.t;
+  if(opt->transaction_fee.set) ctx->transaction_fee = opt->transaction_fee.d;
+  if(opt->csv_output.set) ctx->csv_output = opt->csv_output.i;
+  
+  return 0;
+}
+
+void engine_v2_run(struct engine_v2 *ctx, engine_v2_feed_ptr feed)
+{
+  struct timeline_slice *slice;
+  __list_for_each__(&ctx->timeline->by_slice, slice)
+    feed(ctx, slice);
+}
---
+++ engine/engine_v2.h
@@ -0,0 +1,37 @@
+/*
+ * Cresus CommonTL - engine_v2.h
+ * 
+ * Created by Joachim Naulet <jnaulet@rdinnovation.fr> on 06/05/2019
+ * Copyright (c) 2014 Joachim Naulet. All rights reserved.
+ *
+ */
+
+#ifndef ENGINE_V2_H
+#define ENGINE_V2_H
+
+#include "framework/timeline.h"
+#include "framework/time_info.h"
+
+#include "engine/common_opt.h"
+
+struct engine_v2 {
+  struct timeline *timeline;
+  /* Positions filter */
+  time_info_t start_time;
+  time_info_t end_time;
+  /* Stats */
+  double transaction_fee; /* Cost per transaction */
+  /* CSV graph output */
+  int csv_output;
+};
+
+/* External pointer to plugin */
+typedef int (*engine_v2_feed_ptr)(struct engine_v2*, struct timeline_slice*);
+
+int engine_v2_init(struct engine_v2 *ctx, struct timeline *t);
+void engine_v2_release(struct engine_v2 *ctx);
+
+int engine_v2_set_common_opt(struct engine_v2 *ctx, struct common_opt *opt);
+void engine_v2_run(struct engine_v2 *ctx, engine_v2_feed_ptr feed);
+
+#endif
